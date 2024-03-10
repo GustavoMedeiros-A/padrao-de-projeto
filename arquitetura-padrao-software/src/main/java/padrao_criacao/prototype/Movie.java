dacb0ce20291cf931407f904d8fdf2118e167fec
Gustavo Medeiros <gustavomedeiros15@outlook.com.br>
2024-03-10 18:08:26 -0300

    feat: prototype pattern

---
+++ arquitetura-padrao-software/src/main/java/padrao_criacao/prototype/Movie.java
@@ -0,0 +1,53 @@
+package padrao_criacao.prototype;
+
+public class Movie implements Cloneable{
+    private String name;
+    private Integer duration;
+    private Category category;
+
+    public Movie(String name, Integer duration, Category category) {
+        this.name = name;
+        this.duration = duration;
+        this.category = category;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public Integer getDuration() {
+        return duration;
+    }
+
+    public void setDuration(Integer duration) {
+        this.duration = duration;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    @Override
+    public Movie clone() throws CloneNotSupportedException {
+        Movie movieClone = (Movie) super.clone();
+        movieClone.category = (Category) movieClone.category.clone();
+        return movieClone;
+    }
+
+    @Override
+    public String toString() {
+        return "Movie {" +
+                "name=" + name +
+                ", duration=" + duration +
+                ", category=" + category +
+                '}';
+    }
+
+
+}
---
+++ arquitetura-padrao-software/src/test/java/padrao_criacao/prototype/MovieTest.java
@@ -0,0 +1,40 @@
+package padrao_criacao.prototype;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MovieTest {
+
+        @Test
+        void testMovie() throws CloneNotSupportedException {
+                Movie movie = new Movie(
+                                "Movie", 10,
+                                new Category("Category1", "This is a category"));
+
+                Movie movieClone = movie.clone();
+                movieClone.setName("Movie2");
+                movieClone.setDuration(20);
+                movieClone.getCategory().setName("New category");
+                movieClone.getCategory().setDescription("New description");
+
+                assertEquals(
+                                "Movie {" +
+                                                "name=Movie, " +
+                                                "duration=10, " +
+                                                "category=Category{" +
+                                                "name='Category1', " +
+                                                "description='This is a category'}}",
+                                movie.toString());
+
+                assertEquals(
+                        "Movie {" +
+                                "name=Movie2, " +
+                                "duration=20, " +
+                                "category=Category{" +
+                                "name='New category', " +
+                                "description='New description'}}",
+                        movieClone.toString());
+        };
+
+}
